7424310312aa4535f4f89ccad48534e6ee393816
Chris Ward <chris@cwardcode.com>
2021-06-11 09:55:19 -0400

    Added account name mapping to the role name in header

--- aws-fix-role.js
+++ aws-fix-role.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/honkyjive/
 // @version      0.1
 // @description  try to take over the world!
-// @author       Erick Gallegos
+// @author       Erick Gallegos, Chris Ward
 // @match        https://console.aws.amazon.com/*
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @grant        none
@@ -12,12 +12,16 @@
 (function() {
     'use strict';
     var value;
+    const accountNameMap = new Map();
+    // TODO - Add each account, and its alias that you would want to have displayed next to the role name
+    accountNameMap.set('123456789123', 'my-account');
     $('*:contains("AWSReservedSSO_")').each(function(){
+        // Find AWS Account ID
+        const accountId = document.querySelectorAll("[data-testid='aws-my-account-details']")[0].innerText;
         if($(this).children().length < 1) {
-            value = $(this).text().split('_')[1];
+            value = `${$(this).text().split('_')[1]} - ${accountNameMap.get(accountId)}`;
             $(this).text(value);
         }
     })
-
 
 })();
